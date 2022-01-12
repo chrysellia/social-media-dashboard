2de5ad4b1ba34093633602afd75b7f32644ea91f
Chrysellia <priscilliachryso@gmail.com>
2022-01-12 16:26:50 +0300

    Position absolute Dashboard component to Header

--- social-media-dashboard/src/components/Page/Page.component.js
+++ social-media-dashboard/src/components/Page/Page.component.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Header from '../Header/Header.component'
 import Dashboard from '../Dashboard/Dashboard.component'
 import Overview from '../Overview/Overview.component'
+import './Page.style.scss'
 
 const Page = () => {
     return (
-        <div>
+        <div className="page-wrapper">
             <Header/>
             <Dashboard/>
             <Overview/>
